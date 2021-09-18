269e3c67f9dcd1de71ffea84b2bb6cca28122a08
Artem Boyarskov <arboyarskov@ya.ru>
2021-09-18 12:39:03 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(double height) {
+        double norma = (height - 100) * 1.15;
+        return norma;
+    }
+
+    public static double womanWeight(double height) {
+        double norma = (height - 110) * 1.15;
+        return norma;
+    }
+
+    public static void main(String[] args) {
+        double manNorma = Fit.manWeight(187);
+        double womanNorma = Fit.womanWeight(160);
+        System.out.println("Man 187 is " + manNorma);
+        System.out.println("Woman 160 is " + womanNorma);
+    }
+
+}
