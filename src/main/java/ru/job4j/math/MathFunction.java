6256f862cd613e3a6682122e18ef24742c5cbace
Artem Boyarskov <arboyarskov@ya.ru>
2021-10-11 14:06:37 +0300

    1.1. Что такое import [#504773 #214655]

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class MathFunction {
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+}
+
