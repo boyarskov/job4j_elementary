39c0024e53fdca4f797487c11d4e4af110cbbb86
Artem Boyarskov <arboyarskov@ya.ru>
2021-10-12 08:56:44 +0300

    1.1. Что такое import [#504773 #214655]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,15 +1,20 @@
 package ru.job4j.calculator;
 
-import ru.job4j.math.MathFunction;
+import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
-        return MathFunction.sum(first, second)
-                + MathFunction.multiply(first, second);
+        return sum(first, second) + multiply(first, second);
+    }
+
+   public static double differenceAndDivision(double first, double second) {
+        return difference(first, second) + division(first, second);
     }
 
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + differenceAndDivision(10, 20));
     }
+
 }
--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -2,12 +2,21 @@ package ru.job4j.math;
 
 public class MathFunction {
     public static double sum(double first, double second) {
-        return first + second;
+                return first + second;
     }
 
     public static double multiply(double first, double second) {
-        return first * second;
+                return first * second;
     }
 
+    //В класс MathFunction добавьте еще 2 метода, которые будут рассчитывать разность и деление двух чисел соответственно.
+
+    public static double difference(double first, double second) {
+        return first - second;
+    }
+
+    public static double division(double first, double second) {
+        return first / second;
+    }
 }
 
