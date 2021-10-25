2e4c35b25a36966f27e9844c6117c22e6825ddaa
Artem Boyarskov <arboyarskov@ya.ru>
2021-10-25 20:35:40 +0300

    4.1.0. Оператор if с блоком else [#227534]

--- src/main/java/ru/job4j/condition/Cinema.java
+++ src/main/java/ru/job4j/condition/Cinema.java
@@ -1,4 +1,18 @@
 package ru.job4j.condition;
 
 public class Cinema {
+
+    public static void dostup(int age) {
+        System.out.println("The age of the customer is : " + age);
+        if (age >= 18) {
+            System.out.println("Welcome to the cinema.");
+        } else {
+            System.out.println("It is not for you.");
+        }
+    }
+
+    public static void main(String[] args) {
+        dostup(21);
+        dostup(16);
+    }
 }
