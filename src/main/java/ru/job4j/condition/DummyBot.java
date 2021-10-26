153b307d88d6157972be59dfbab8de175062640e
Artem Boyarskov <arboyarskov@ya.ru>
2021-10-26 20:03:33 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if (question.equals("Привет, Бот.")) {
+        rsl = "Привет, умник.";
+        }
+        if (question.equals("Пока.")) {
+        rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+    }
+}
