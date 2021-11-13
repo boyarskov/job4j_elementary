d851c9f6b089676480f45a4979810d7f98ef83f4
Artem Boyarskov <arboyarskov@ya.ru>
2021-11-13 14:23:43 +0300

    5.5. Простое число [#171694]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,23 +3,15 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
-        for (int index = 2; index < (number - 1); index++) { //начинаем перебор цифр с 2 до (number - 1)
-            if (number % index == 0) { //если number делится на index без остатка, то число НЕ простое
+        for (int index = 2; index < (number - 1); index++) {
+            if (number % index == 0) {
                 System.out.println("Число = " + number + ". не простое. Остановка");
-                prime = false; //и мы возвращаем prime = false
+                prime = false;
                 break;
             } else {
                 System.out.println("Число = " + number + ". простое. WIN");
-                break;
             }
         }
         return prime;
-    }
-
-    public static void main(String[] args) {
-        CheckPrimeNumber.check(6);
-        CheckPrimeNumber.check(34);
-        CheckPrimeNumber.check(9);
-        CheckPrimeNumber.check(5);
     }
 }
