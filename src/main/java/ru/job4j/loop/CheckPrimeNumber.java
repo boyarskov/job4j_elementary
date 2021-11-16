77d9b4c305e9406204b47b07fcaa84ae529cf93c
Artem Boyarskov <arboyarskov@ya.ru>
2021-11-16 09:17:37 +0300

    5.6. Простые числа [#156314 #228903]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,7 +5,6 @@ public class CheckPrimeNumber {
         boolean prime = number > 1;
         for (int index = 2; index < number; index++) {
             if (number % index == 0) {
-                System.out.println("Число = " + number + ". не простое. Остановка");
                 prime = false;
                 break;
             }
