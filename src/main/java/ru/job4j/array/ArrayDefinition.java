a3aa331a76686d39036c406250f94cdbcf8674c6
Artem Boyarskov <arboyarskov@ya.ru>
2021-11-18 18:59:41 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен : " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен : " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Slava";
+        names[1] = "Artem";
+        names[2] = "Elena";
+        names[3] = "Anastasia";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
