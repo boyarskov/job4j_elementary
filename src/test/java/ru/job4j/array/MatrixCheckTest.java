bbeca6b20d8e7309bab043c1e59e757242868072
Artem Boyarskov <arboyarskov@ya.ru>
2021-12-04 14:13:37 +0300

    6.7.2. Моно столбец в матрице. [#214127]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -7,9 +7,9 @@ public class MatrixCheckTest {
     @Test
     public void whenHasMonoHorizontal() {
         char[][] input = {
-                {' ', ' ', ' '},
-                {'X', 'X', 'X'},
-                {'X', ' ', ' '},
+                {' ', ' ', ' ' },
+                {'X', 'X', 'X' },
+                {'X', ' ', ' ' },
         };
         int row = 1;
         boolean result = MatrixCheck.monoHorizontal(input, row);
@@ -19,12 +19,36 @@ public class MatrixCheckTest {
     @Test
     public void whenNotHasMonoHorizontal() {
         char[][] input = {
-                {' ', 'X', ' '},
-                {' ', 'X', 'X'},
-                {' ', ' ', ' '},
+                {' ', 'X', ' ' },
+                {' ', 'X', 'X' },
+                {' ', ' ', ' ' },
         };
         int row = 1;
         boolean result = MatrixCheck.monoHorizontal(input, row);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean result = MatrixCheck.monoVertical(input, column);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenNotHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', ' '},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean result = MatrixCheck.monoVertical(input, column);
         Assert.assertFalse(result);
     }
 }
